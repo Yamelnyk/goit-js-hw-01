a48d65dc49e9e754f0806577b68478842efc8f60
Yamelnyk <yachkana@ukr.net>
2024-01-12 23:07:31 +0200

    all

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,10 @@
 function getElementWidth(content, padding, border) {
-    const totalWidth = `${Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2}`;
+    const totalWidth = parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
+    console.log(typeof totalWidth)
     return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
+
